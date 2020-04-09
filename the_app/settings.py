a86446c406e6804f42cd8c482f4cc8fae7abd906
Senku1 <morhaliuk@gmail.com>
2020-04-09 21:25:30 +0300

    requirements.txt added; set .env file

--- the_app/settings.py
+++ the_app/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ENV_PATH = BASE_DIR + '/.env'
+load_dotenv(dotenv_path=ENV_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p3jo^igt*93!*up(el@t=moq2*d@bd8$2pd&xvp3lo5p7_d2iw'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
